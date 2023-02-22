b0d1ba361fc51fa7efe9329ef8fa77484fdf3143
Alex Lane <97alane@gmail.com>
2023-02-22 13:01:11 -0600

    Navbar and homepage
    basic setup for Navbar and Homepages

--- Demo/inspire-demo/src/App.js
+++ Demo/inspire-demo/src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+
+import ExampleCard from './components/cards';
+import Home from './pages/homepage';
 import './App.css';
 
 function App() {
   return (
+  
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Home />
+
+        
+        <ExampleCard />
       </header>
     </div>
   );
---
+++ Demo/inspire-demo/src/pages/homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import { Card, CardGroup, ListGroup, Container, Row, Col } from 'react-bootstrap'
+import MainNav from '../components/navbar';
+
+
+const Home = () => {
+    return (
+    <>
+     <MainNav />
+        
+    </>
+    );
+};
+
+export default Home
